da1bcecb27bb44598358a2be30cc4afc31715e02
sanu kumar <snkmr13@gmail.com>
2023-07-20 11:45:08 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-/*
- * <div id="parent">
-        <div id="child1">
-            <h1> I am h1 tag </h1>
-            <h2> I am h2 tag</h2>
-        <div>
-        <div id="child2>
-            <h1> I am h1 tag </h1>
-            <h2> I am h2 tag</h2>
-        <div>
-    </div>
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "I am h1 tag 🚀"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel (https://parceljs.org)
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundles
+- Compressing
+- Consistent Hasing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagonostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
